c5519d63e216d0c2a51d558be4c30d1227402d6a
Maksim Parfenenka <138598823+makszeus@users.noreply.github.com>
2023-07-25 22:54:16 +0300

    Create bubble-sort.rs

---
+++ array-sorting-algorithms/selection-sort/bubble-sort.rs
@@ -0,0 +1,15 @@
+fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    let n = arr.len();
+    for i in 0..n {
+        let mut swapped = false;
+        for j in 0..n - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        if !swapped {
+            break;
+        }
+    }
+}
